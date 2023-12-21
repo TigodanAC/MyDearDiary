b744455a44832f8d43f365a42ba17c83bcc01e35
lavsasha <lavickaaaleksandra3@gmail.com>
2023-12-21 17:52:27 +0300

    Добавила запросы для записей

--- data_requests.py
+++ data_requests.py
@@ -32,12 +32,93 @@ def add_user(user_id, sex, age, height, weight):
         combined_data.to_csv("users.csv", index=False)
 
 
+def add_note(user_id, note_name, note_text):
+    csv_data = pd.read_csv("notes.csv")
+    csv_copy = csv_data.copy()
+    note_num = 1
+    for index, row in csv_copy.iterrows():
+        if row['user_id'] == str(user_id):
+            note_num += 1
+    current_datetime = datetime.now().strftime('%d.%m.%Y %H.%M.%S')
+    new_row = {'user_id': user_id, 'note_num': note_num, 'note_date': current_datetime, 'note_name': note_name,
+               'note_text': note_text}
+    new_data = pd.DataFrame([new_row])
+    combined_data = pd.concat([csv_data, new_data], ignore_index=True)
+    combined_data.to_csv("notes.csv", index=False)
+
+
+def get_all_user_notes(user_id):
+    csv_data = pd.read_csv("notes.csv")
+    res_data = list()
+    for index, row in csv_data.iterrows():
+        if row['user_id'] == str(user_id):
+            cur = list()
+            cur.append(row['note_num'])
+            cur.append(row['note_date'])
+            cur.append(row['note_name'])
+            res_data.append(cur)
+
+    return res_data
+
+
+def get_note_by_number(user_id, note_number):
+    csv_data = pd.read_csv("notes.csv")
+    user_data = {}
+    for index, row in csv_data.iterrows():
+        if row['user_id'] == str(user_id):
+            user_data[row['note_num']] = row['note_text']
+
+    if note_number > len(user_data):
+        if len(user_data):
+            res = "Записи с таким номером не существует"
+        else:
+            res = "У Вас нет записей"
+    else:
+        res = user_data[note_number]
+
+    return res
+
+
+def clean_all_user_notes(user_id):
+    csv_data = pd.read_csv("notes.csv")
+    csv_data = csv_data[csv_data['user_id'] != user_id]
+    csv_data.to_csv("notes.csv", index=False)
+
+
+def delete_note_by_number(user_id, note_number):
+    ...
+
+
 def request(type, argc, argv):
     if type == 'GET':
         if argc == 2 and argv[0] == 'user':
             return is_user_exist(argv[1])
+
+        elif argc == 2 and argv[0] == 'note':
+            return get_all_user_notes(argv[1])
+
+        elif argc == 3 and argv[0] == 'note':
+            return get_note_by_number(argv[1], argv[2])
+
         else:
             print("No such GET request")
+
     elif type == 'PUT':
         if argc == 6 and argv[0] == 'user':
             add_user(argv[1], argv[2], argv[3], argv[4], argv[5])
+
+        elif argc == 4 and argv[0] == 'note':
+            add_note(argv[1], argv[2], argv[3])
+
+        else:
+            print("No such PUT request")
+
+    elif type == 'DELETE':
+        if argc == 2 and argv[0] == 'note':
+            return clean_all_user_notes(argv[1])
+
+        elif argc == 3 and argv[0] == 'note':
+            return delete_note_by_number(argv[1], argv[2])
+
+        else:
+            print("No such DELETE request")
