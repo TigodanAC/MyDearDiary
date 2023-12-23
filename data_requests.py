5ea2334ad2b4aa098862ecbf8b05fdabead024d1
lavsasha <lavickaaaleksandra3@gmail.com>
2023-12-23 15:46:15 +0300

    Добавила работу со списками блюд

--- data_requests.py
+++ data_requests.py
@@ -1,3 +1,4 @@
+import ast
 from io import BytesIO
 
 import pandas as pd
@@ -97,6 +98,219 @@ def delete_note_by_number(user_id, note_number):
         return False
 
 
+def add_dish_list(user_id, list_title, dish_names):
+    csv_data = pd.read_csv("wishlist_dishes.csv")
+    csv_copy = csv_data.copy()
+    list_num = 1
+    for index, row in csv_copy.iterrows():
+        if row['user_id'] == str(user_id):
+            list_num += 1
+    dishes_dict = {}
+    csv_dishes = pd.read_csv("dishes.csv")
+    for dish in dish_names:
+        for index, row in csv_dishes.iterrows():
+            if row['meals_names'] == str(dish):
+                dishes_dict[row['id_meals']] = str(dish)
+                break
+    if len(dishes_dict):
+        new_row = {'user_id': user_id, 'list_dish_number': str(list_num), 'list_dish_title': list_title,
+                   'list_dish_names': dishes_dict}
+        new_data = pd.DataFrame([new_row])
+        combined_data = pd.concat([csv_data, new_data], ignore_index=True)
+        combined_data[['user_id', 'list_dish_number', 'list_dish_title', 'list_dish_names']].to_csv(
+            "wishlist_dishes.csv", index=False)
+        return True
+    return False
+
+
+def add_book_list(user_id, list_title, book_names):
+    csv_data = pd.read_csv("wishlist_books.csv")
+    csv_copy = csv_data.copy()
+    list_num = 1
+    for index, row in csv_copy.iterrows():
+        if row['user_id'] == str(user_id):
+            list_num += 1
+    books_dict = {}
+    csv_books = pd.read_csv("micro_books.csv")
+    for book in book_names:
+        for index, row in csv_books.iterrows():
+            if row['names_books'] == str(book):
+                books_dict[row['authors_books']] = str(book)
+                break
+    if len(books_dict):
+        new_row = {'user_id': user_id, 'list_book_number': str(list_num), 'list_book_title': list_title,
+                   'list_book_names': books_dict}
+        new_data = pd.DataFrame([new_row])
+        combined_data = pd.concat([csv_data, new_data], ignore_index=True)
+        combined_data[['user_id', 'list_book_number', 'list_book_title', 'list_book_names']].to_csv(
+            "wishlist_books.csv", index=False)
+        return True
+    return False
+
+
+def add_film_list(user_id, list_title, film_names):
+    csv_data = pd.read_csv("wishlist_films.csv")
+    csv_copy = csv_data.copy()
+    list_num = 1
+    for index, row in csv_copy.iterrows():
+        if row['user_id'] == str(user_id):
+            list_num += 1
+    films_dict = {}
+    csv_films = pd.read_csv("micro_films.csv")
+    for film in film_names:
+        for index, row in csv_films.iterrows():
+            if row['names_films'] == str(film):
+                films_dict[row['years_films']] = str(film)
+                break
+    if len(films_dict):
+        new_row = {'user_id': user_id, 'list_film_number': str(list_num), 'list_film_title': list_title,
+                   'list_film_names': films_dict}
+        new_data = pd.DataFrame([new_row])
+        combined_data = pd.concat([csv_data, new_data], ignore_index=True)
+        combined_data[['user_id', 'list_film_number', 'list_film_title', 'list_film_names']].to_csv(
+            "wishlist_films.csv", index=False)
+        return True
+    return False
+
+
+def add_in_dish_list_by_number(user_id, list_number, dish_names):
+    csv_data = pd.read_csv("wishlist_dishes.csv")
+    csv_dishes = pd.read_csv("dishes.csv")
+    flag = False
+    for index, row in csv_data.iterrows():
+        if row['user_id'] == str(user_id) and str(row['list_dish_number']) == str(list_number):
+            dishes_dict = eval(row['list_dish_names'])
+            for dish in dish_names:
+                for idx, r in csv_dishes.iterrows():
+                    if r['meals_names'] == str(dish):
+                        flag = True
+                        dishes_dict[r['id_meals']] = str(dish)
+                        break
+            csv_data.at[index, 'list_dish_names'] = str(dishes_dict)
+    csv_data.to_csv("wishlist_dishes.csv", index=False)
+    return flag
+
+
+# Непонятно, что делать в случае нескольких списков с одним названием"
+def add_in_dish_list_by_title(user_id, list_title, dish_names):
+    csv_data = pd.read_csv("wishlist_dishes.csv")
+    csv_dishes = pd.read_csv("dishes.csv")
+    flag = False
+    for index, row in csv_data.iterrows():
+        if row['user_id'] == str(user_id) and row['list_dish_title'] == str(list_title):
+            dishes_dict = eval(row['list_dish_names'])
+            for dish in dish_names:
+                for idx, r in csv_dishes.iterrows():
+                    if r['meals_names'] == str(dish):
+                        flag = True
+                        dishes_dict[r['id_meals']] = str(dish)
+                        break
+            csv_data.at[index, 'list_dish_names'] = str(dishes_dict)
+    csv_data.to_csv("wishlist_dishes.csv", index=False)
+    return flag
+
+
+def delete_dish_list_by_number(user_id, list_number):
+    csv_data = pd.read_csv("wishlist_dishes.csv")
+    deleted = False
+    for index, row in csv_data.iterrows():
+        if row['user_id'] == str(user_id) and int(row['list_dish_number']) == list_number:
+            csv_data = csv_data.drop(index)
+            deleted = True
+        elif deleted and row['user_id'] == str(user_id):
+            csv_data.at[index, 'list_dish_number'] = int(row['list_dish_number']) - 1
+    if deleted:
+        csv_data.to_csv("wishlist_dishes.csv", index=False)
+        return True
+    return False
+
+
+# Непонятно, что делать в случае нескольких списков с одним названием
+def delete_dish_list_by_title(user_id, list_title):
+    csv_data = pd.read_csv("wishlist_dishes.csv")
+    list_num_to_update = None
+    for index, row in csv_data.iterrows():
+        if row['user_id'] == str(user_id) and row['list_dish_title'] == str(list_title):
+            list_num_to_update = int(row['list_dish_number'])
+            csv_data = csv_data.drop(index)
+    if list_num_to_update is not None:
+        for index, row in csv_data.iterrows():
+            if row['user_id'] == str(user_id) and int(row['list_dish_number']) > list_num_to_update:
+                csv_data.at[index, 'list_dish_number'] = int(row['list_dish_number']) - 1  # Cast to int
+        csv_data.to_csv("wishlist_dishes.csv", index=False)
+        return True
+    return False
+
+
+def delete_in_dish_list_by_number(user_id, list_number, dish_names):
+    csv_data = pd.read_csv("wishlist_dishes.csv")
+    for index, row in csv_data.iterrows():
+        if row['user_id'] == str(user_id) and str(row['list_dish_number']) == str(list_number):
+            dishes_dict = eval(row['list_dish_names'])
+            for dish in dish_names:
+                for key, value in list(dishes_dict.items()):
+                    if value == dish:
+                        del dishes_dict[key]
+            csv_data.at[index, 'list_dish_names'] = str(dishes_dict)
+    csv_data.to_csv("wishlist_dishes.csv", index=False)
+
+
+# Непонятно, что делать в случае нескольких списков с одним названием"
+def delete_in_dish_list_by_title(user_id, list_title, dish_names):
+    csv_data = pd.read_csv("wishlist_dishes.csv")
+    for index, row in csv_data.iterrows():
+        if row['user_id'] == str(user_id) and row['list_dish_title'] == str(list_title):
+            dishes_dict = eval(row['list_dish_names'])
+            for dish in dish_names:
+                for key, value in list(dishes_dict.items()):
+                    if value == dish:
+                        del dishes_dict[key]
+            csv_data.at[index, 'list_dish_names'] = str(dishes_dict)
+    csv_data.to_csv("wishlist_dishes.csv", index=False)
+
+
+def clean_all_user_dish_lists(user_id):
+    csv_data = pd.read_csv("wishlist_dishes.csv")
+    csv_data = csv_data[csv_data['user_id'] != user_id]
+    csv_data.to_csv("wishlist_dishes.csv", index=False)
+
+
+def get_all_user_dish_lists(user_id):
+    csv_data = pd.read_csv("wishlist_dishes.csv")
+    csv_data = csv_data[csv_data['user_id'] == str(user_id)]
+    csv_data.drop('list_dish_names', axis=1, inplace=True)
+    csv_data.drop('user_id', axis=1, inplace=True)
+    new_column_names = {'list_dish_number': 'Номер', 'list_dish_title': 'Название списка'}
+    df = csv_data.rename(columns=new_column_names).reset_index(drop=True)
+    df_styled = df.style.set_properties(**{'text-align': 'center'}).hide()
+    buf = BytesIO()
+    dfi.export(df_styled, buf)
+    return buf
+
+
+def get_dish_list_by_number(user_id, list_number):
+    csv_data = pd.read_csv("wishlist_dishes.csv")
+    dishes_list = []
+    for index, row in csv_data.iterrows():
+        if row['user_id'] == user_id and int(row['list_dish_number']) == list_number:
+            dishes_dict = ast.literal_eval(row['list_dish_names'])
+            dishes_list = [dishes_dict[key] for key in dishes_dict]
+            break
+    return dishes_list
+
+
+# Непонятно, что делать в случае нескольких списков с одним названием"
+def get_dish_list_by_title(user_id, list_title):
+    csv_data = pd.read_csv("wishlist_dishes.csv")
+    dishes_list = []
+    for index, row in csv_data.iterrows():
+        if row['user_id'] == user_id and str(row['list_dish_title']) == str(list_title):
+            dishes_dict = ast.literal_eval(row['list_dish_names'])
+            dishes_list = [dishes_dict[key] for key in dishes_dict]
+            break
+    return dishes_list
+
+
 def request(type, argc, argv):
     if type == 'GET':
         if argc == 2 and argv[0] == 'user':
@@ -108,6 +322,15 @@ def request(type, argc, argv):
         elif argc == 3 and argv[0] == 'note':
             return get_note_by_number(argv[1], argv[2])
 
+        elif argc == 2 and argv[0] == 'dish_list':
+            return get_all_user_dish_lists(argv[1])
+
+        elif argc == 3 and argv[0] == 'dish_list_n':
+            return get_dish_list_by_number(argv[1], argv[2])
+
+        elif argc == 3 and argv[0] == 'dish_list_t':
+            return get_dish_list_by_title(argv[1], argv[2])
+
         else:
             print("No such GET request")
 
@@ -118,6 +341,21 @@ def request(type, argc, argv):
         elif argc == 4 and argv[0] == 'note':
             add_note(argv[1], argv[2], argv[3])
 
+        elif argc == 4 and argv[0] == 'dish_list':
+            add_dish_list(argv[1], argv[2], argv[3])
+
+        elif argc == 4 and argv[0] == 'book_list':
+            add_book_list(argv[1], argv[2], argv[3])
+
+        elif argc == 4 and argv[0] == 'film_list':
+            add_film_list(argv[1], argv[2], argv[3])
+
+        elif argc == 4 and argv[0] == 'dish_list_n':
+            add_in_dish_list_by_number(argv[1], argv[2], argv[3])
+
+        elif argc == 4 and argv[0] == 'dish_list_t':
+            add_in_dish_list_by_title(argv[1], argv[2], argv[3])
+
         else:
             print("No such PUT request")
 
@@ -127,6 +365,21 @@ def request(type, argc, argv):
 
         elif argc == 3 and argv[0] == 'note':
             return delete_note_by_number(argv[1], argv[2])
+
+        elif argc == 2 and argv[0] == 'dish_list':
+            return clean_all_user_dish_lists(argv[1])
+
+        elif argc == 3 and argv[0] == 'dish_list_n':
+            delete_dish_list_by_number(argv[1], argv[2])
+
+        elif argc == 3 and argv[0] == 'dish_list_t':
+            delete_dish_list_by_title(argv[1], argv[2])
+
+        elif argc == 4 and argv[0] == 'dish_list_n':
+            delete_in_dish_list_by_number(argv[1], argv[2], argv[3])
+
+        elif argc == 4 and argv[0] == 'dish_list_t':
+            delete_in_dish_list_by_title(argv[1], argv[2], argv[3])
 
         else:
             print("No such DELETE request")
--- micro_data.py
+++ micro_data.py
@@ -5,7 +5,7 @@ types_books = ["Классическая русская литература", "
 authors_books =["Достоевский Федор Михайлович", "Хейзелвуд Али"]
 rating_books = [4.7, 4.4]
 links_books = ["https://book24.ru/product/besy-5422017/", "https://book24.ru/product/shakh-i-mat-6784435/"]
-dictionary1 = {"Название": names_books, "Aвтор": authors_books, "Раздел": types_books, "Рейтинг": rating_books, "Узнать больше": links_books}
+dictionary1 = {"names_books": names_books, "authors_books": authors_books, "types_books": types_books, "rating_books": rating_books, "links_books": links_books}
 
 
 names_films = ["Зверополис", "Поезд в Пусан"]
@@ -13,7 +13,7 @@ types_films = ["Мультфильм", "Ужасы, Боевик"]
 rating_films = [8.3, 7.2]
 years_films = ["2016", "2016"]
 links_films = ["https://www.kinopoisk.ru/film/775276/", "https://www.kinopoisk.ru/film/977288/"]
-dictionary2 = {"Название": names_films, "Жанр": types_films, "Рейтинг": rating_films, "Год выпуска": years_films, "Узнать больше": links_films}
+dictionary2 = {"names_films": names_films, "types_film": types_films, "rating_films": rating_films, "years_films": years_films, "links_films": links_films}
 
 df1 = pd.DataFrame(dictionary1)
 df1.to_csv('micro_books.csv', index=False)
